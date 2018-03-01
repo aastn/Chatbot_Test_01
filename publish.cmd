075a19f89661b5cd293bf80dfe021d0cff10fb1a
aastn <36929784+aastn@users.noreply.github.com>
2018-03-01 12:26:54 +0100

    Initial Git Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=aastntest01-Web-Deploy.pubxml -p:Password=aKro8u3L4orvJSk5p7PieKEAtjYzKEKtpX5aQPjj2LXnBrperj5EldddnqJp
+
